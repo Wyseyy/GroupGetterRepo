fda355fd20e4dc7c611ea15104972e47b530c605
Your Name <my@email.org>
2024-12-16 11:41:06 +0000

    Midpoint

--- app/src/main/java/com/example/groupgetter/CServiceActivity.java
+++ app/src/main/java/com/example/groupgetter/CServiceActivity.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class CServiceActivity extends AppCompatActivity {
@@ -16,33 +17,36 @@ public class CServiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cservice); // This connects to activity_cservice.xml
 
+        bottomMenu = findViewById(R.id.navigation_gg);
 
-        bottomMenu.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        // Check which item in the BottomNavigationView was selected
-                        switch (item.getItemId()) {
-                            // If the Home item was selected
-                            case R.id.navigation_home:
-                                // Create an Intent to launch the HomeActivity
-                                Intent homeIntent = new Intent(CServiceActivity.this, HomePageActivity.class);
-                                // Start the HomeActivity
-                                startActivity(homeIntent);
-                                return true;
-                            // If the Community item was selected
-                            case R.id.navigation_community:
-                                // Create an Intent to launch the CommunityActivity
-                                Intent communityIntent = new Intent(CServiceActivity.this, CommunityActivity.class);
-                                // Start the CommunityActivity
-                                startActivity(communityIntent);
-                                return true;
-                            //If no item was selected, return false
-                            default:
-                                return false;
-                        }
-                    }
-                });
+        bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Check which item in the BottomNavigationView was selected
+                switch (item.getItemId()) {
+                    // If the Home item was selected
+                    case R.id.navigation_home:
+                        // Create an Intent to launch the HomeActivity
+                        Intent homeIntent = new Intent(CServiceActivity.this, HomePageActivity.class);
+                        // Start the HomeActivity
+                        startActivity(homeIntent);
+                        return true;
+
+                    // If the Community item was selected
+                    case R.id.navigation_community:
+                        // Create an Intent to launch the CommunityActivity
+                        Intent communityIntent = new Intent(CServiceActivity.this, CommunityActivity.class);
+                        // Start the CommunityActivity
+                        startActivity(communityIntent);
+                        return true;
+
+                    // If no item was selected, return false
+                    default:
+                        return false;
+                }
+            }
+        });
+
     }
 
 
--- app/src/main/java/com/example/groupgetter/CommunityActivity.java
+++ app/src/main/java/com/example/groupgetter/CommunityActivity.java
@@ -7,7 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import com.google.android.material.navigation.NavigationBarView;
 public class CommunityActivity extends AppCompatActivity {
     private BottomNavigationView bottomMenu;
     @Override
@@ -15,31 +15,34 @@ public class CommunityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community); // This connects to activity_community.xml
 
-        bottomMenu.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        // Check which item in the BottomNavigationView was selected
-                        switch (item.getItemId()) {
-                            // If the Home item was selected
-                            case R.id.navigation_home:
-                                // Create an Intent to launch the HomeActivity
-                                Intent homeIntent = new Intent(CommunityActivity.this, HomePageActivity.class);
-                                // Start the HomeActivity
-                                startActivity(homeIntent);
-                                return true;
-                            // If the Community item was selected
-                            case R.id.navigation_cservice:
-                                // Create an Intent to launch the CServiceActivity
-                                Intent cserviceIntent = new Intent(CommunityActivity.this, CServiceActivity.class);
-                                // Start the CServiceActivity
-                                startActivity(cserviceIntent);
-                                return true;
-                            //If no item was selected, return false
-                            default:
-                                return false;
-                        }
-                    }
-                });
+        bottomMenu = findViewById(R.id.navigation_gg);
+        bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Check which item in the BottomNavigationView was selected
+                switch (item.getItemId()) {
+                    // If the Home item was selected
+                    case R.id.navigation_home:
+                        // Create an Intent to launch the HomeActivity
+                        Intent homeIntent = new Intent(CommunityActivity.this, HomePageActivity.class);
+                        // Start the HomeActivity
+                        startActivity(homeIntent);
+                        return true;
+
+                    // If the CService item was selected
+                    case R.id.navigation_cservice:
+                        // Create an Intent to launch the CServiceActivity
+                        Intent cserviceIntent = new Intent(CommunityActivity.this, CServiceActivity.class);
+                        // Start the CServiceActivity
+                        startActivity(cserviceIntent);
+                        return true;
+
+                    // If no item was selected, return false
+                    default:
+                        return false;
+                }
+            }
+        });
+
     }
 }
