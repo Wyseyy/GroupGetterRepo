22f0640d477a307b358112668c9a636fcd2fadfe
Wyseyy <eoinwysey@gmail.com>
2025-02-19 00:38:18 +0000

    Midpoint

--- app/src/main/java/com/example/groupgetter/MainActivity.java
+++ app/src/main/java/com/example/groupgetter/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.groupgetter;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import android.view.MenuItem;
-import android.content.Intent;
-import android.util.Log;
-import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomMenu;
+    private EditText searchEditText;
+    private Button searchButton;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         finish();
 
         bottomMenu = findViewById(R.id.navigation_gg);
+
 
 
         // Set the OnNavigationItemSelectedListener of the BottomNavigationView
